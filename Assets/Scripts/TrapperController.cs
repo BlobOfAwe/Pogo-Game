1487c1be3bf8ce5b54f08c8d3d80d815d4c2eda3
ZeeTheBeee <s.sheikhanzai@gmail.com>
2023-03-31 13:57:49 -0700

    More fixes

--- Assets/Scripts/TrapperController.cs
+++ Assets/Scripts/TrapperController.cs
@@ -6,13 +6,24 @@ public class TrapperController : MonoBehaviour
 {
     private BoxCollider2D bc;
     private TrapperStunScript trapperStun;
+    private Animator trapperAnimator;
     // Start is called before the first frame update
     void Start()
     {
+        trapperAnimator = GetComponentInParent<Animator>();
         bc = GetComponent<BoxCollider2D>();
         trapperStun = FindObjectOfType<TrapperStunScript>();
     }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //Checks if the tag is assigned as Player
+        if (other.gameObject.tag == "Player")
+        {
+            //Initiates the stun function
+           trapperAnimator.SetTrigger("IsBiting");
+        }
 
+    }
     // Update is called once per frame
     void Update()
     {
