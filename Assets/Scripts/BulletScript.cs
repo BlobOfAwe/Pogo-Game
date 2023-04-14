90836cb220b2a0794bf134edd9eab7c8f4b06221
BlobOfAwe <jamescvm03@gmail.com>
2023-04-14 02:03:41 -0700

    Update BulletScript.cs
    Bullets now make sound

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -8,17 +8,26 @@ public class BulletScript : MonoBehaviour
     /// this script handles the colissions of the bullet
     /// if it collides with the ground/wall or player then it destroys itself
     /// </summary>
+    private AudioClip aud;
+    private DamagePlayer dmgPlr;
+    private CircleCollider2D col;
+    private SpriteRenderer spr;
+
+    private void Start()
+    {
+        dmgPlr = GetComponent<DamagePlayer>();
+        col = GetComponent<CircleCollider2D>();
+        spr = GetComponent<SpriteRenderer>();
+        aud = GetComponent<AudioSource>().clip;
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == ("Ground"))
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Ground"))
         {
-            Destroy(gameObject);
-        }
-        else if (other.gameObject.tag == ("Player"))
-        {
-            Destroy(gameObject);
-
-            //damage and knock back the player
+            dmgPlr.enabled = false;
+            col.enabled = false;
+            spr.enabled = false;
+            Destroy(gameObject, aud.length);
         }
     }
 }
